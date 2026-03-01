ee70da78a76f1cdb852dabbad4448bb5a4b4e1d6
Nathan Kenney <nake73a@gmail.com>
2026-03-01 15:13:55 -0500

    Added Privacy Policy as well as updated carousel

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/schedule-a-quote')
+def quote():
+    return render_template('quote.html')
 
+@app.route('/privacy-policy')
+def privacy_policy():
+    return render_template('privacy-policy.html')
+
+# Implement a event tab that updates with current giveaways and etc
 if __name__ == '__main__':
     app.run()
