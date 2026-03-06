350c6eadb046b443ac19f236735c1c0b191b5f10
proximalbeet <97807056+proximalbeet@users.noreply.github.com>
2026-03-06 17:19:41 -0500

    Updated port to 8080

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ def submit_quote():
 
 # Implement a event tab that updates with current giveaways and etc
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
